fee9ff64630259159c9de443484b335c6f64e238
Daksh Gupta <61038855+Daksh-404@users.noreply.github.com>
2020-09-30 23:36:10 +0530

    Update keypoint.h

--- SIFTapp/keypoint.h
+++ SIFTapp/keypoint.h
@@ -1,3 +1,11 @@
+/****************************************************** 
+ *	Code by Utkarsh Sinha
+ *	Based on JIFT by Jun Liu
+ *	Visit http://aishack.in/ for more indepth articles and tutorials
+ *	on artificial intelligence
+ * Use, reuse, modify, hack, kick. Do whatever you want with
+ * this code :)
+ ******************************************************/
 #pragma once
 #ifndef _KEYPOINT_H
 #define _KEYPOINT_H
