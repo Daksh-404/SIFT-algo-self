e6cdb926d6316a6d9c86ef7705680c447432ad35
Daksh Gupta <61038855+Daksh-404@users.noreply.github.com>
2020-09-30 23:38:35 +0530

    Update SIFT.cpp

--- SIFT.cpp
+++ SIFT.cpp
@@ -1,3 +1,11 @@
+/****************************************************** 
+ *	Code by Utkarsh Sinha
+ *	Based on JIFT by Jun Liu
+ *	Visit http://aishack.in/ for more indepth articles and tutorials
+ *	on artificial intelligence
+ * Use, reuse, modify, hack, kick. Do whatever you want with
+ * this code :)
+ ******************************************************/
 #include<iostream>
 #include<opencv2/opencv.hpp>
 #include "mySIFT.h"
